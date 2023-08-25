090ce996f5aad627acdddfc2c37ac372e69edcf4
Diego Rockenbach <rdiegomatias@gmail.com>
2023-08-25 19:42:25 -0300

    Commit parcial dos exercícios realizados no dia 24 de agosto

---
+++ 08-24-23/Exec5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+float a, b, c, delta, raizNeg, raizPos, raizUnica;
+int main(){
+
+  printf("\nPressupondo uma equação de segundo grau, insira o valor de A:\n");
+  scanf("%f", &a);
+  printf("\nAgora, insira o valor de B:\n");
+  scanf("%f", &b);
+  printf("\nE, por último, insira o valor de C:\n");
+  scanf("%f", &c);
+  
+  delta = (pow(b,2)-4*a*c); // Equação de segundo grau: (-b±√b²-4ac)/2a
+
+  if (delta<0) {
+    printf("\nA equação com os valores inseridos não tem raízes.\n\n");
+  }
+  else if (a==0) {
+    raizUnica = b/(c*(-1));
+    5x - 5 = 0
+    5x = 5
+
+    printf("\n\nVisto que o valor inserido para A foi zero, forma-se uma equação de primeiro grau, com sua única raiz sendo %.2f", raizUnica);
+  }
+  else {
+    delta = sqrt(delta);
+    raizPos = ((-b)+delta)/(2*a);
+    raizNeg = ((-b)-delta)/(2*a);
+    printf("\nA raíz positiva da equação em questão é %.2f, e a raiz negativa é %.2f.\n\n", raizPos, raizNeg);
+  }
+
+  system("pause");
+  return 0;
+}
